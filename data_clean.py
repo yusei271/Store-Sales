b5e258d666295f227230d076f52667617ff107cd
omiu <m.ohara.24.7.1545@4nonome.com>
2025-03-06 17:32:50 +0900

    data_clean2

--- data_clean.py
+++ data_clean.py
@@ -1,3 +1,48 @@
+import numpy as np  # linear algebra
+import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+#from sklearn.decomposition import PCA
+
+# データを読み込む
+df_train = pd.read_csv("train.csv")
+df_oil = pd.read_csv("oil.csv")  # 修正箇所
+df_stores = pd.read_csv("stores.csv")  # 他のデータも必要なら読み込む
+df_test = pd.read_csv("test.csv")
+#追加
+df_holidays = pd.read_csv('holidays_events.csv')
+df_transactions = pd.read_csv('transactions.csv')
+sample_subm = pd.read_csv('sample_submission.csv')
+
+# 日付をdatetime型に変換
+df_train['date'] = pd.to_datetime(df_train['date'], format="%Y-%m-%d")
+print(df_train['date'].head())
+df_oil['date'] = pd.to_datetime(df_oil['date'], format="%Y-%m-%d")
+print(df_oil['date'].head())
+df_test['date'] = pd.to_datetime(df_test['date'], format="%Y-%m-%d")
+print(df_test['date'].head())
+df_holidays['date'] = pd.to_datetime(df_holidays['date'], format="%Y-%m-%d")
+print(df_holidays['date'].head())
+#追加
+df_holidays['date'] = pd.to_datetime(df_train['date'], format="%Y-%m-%d")
+df_transactions['date'] = pd.to_datetime(df_train['date'], format="%Y-%m-%d")
+
+
+# データをマージ
+df_train = pd.merge(df_train, df_stores, on="store_nbr", how="left")
+df_train = pd.merge(df_train, df_transactions, on=["store_nbr", "date"], how="left")
+df_train = pd.merge(df_train, df_holidays, on="date", how="left")
+df_train = pd.merge(df_train, df_oil, on="date", how="left")
+
+df_test = pd.merge(df_test, df_stores, on="store_nbr", how="left")
+df_test = pd.merge(df_test, df_transactions, on=["store_nbr", "date"], how="left")
+df_test = pd.merge(df_test, df_holidays, on="date", how="left")
+df_test = pd.merge(df_test, df_oil, on="date", how="left")
+print(df_train.head())
+print(df_test.head())
+#print(df_train.head())
+
 # 欠損値の割合を計算
 print(np.round(df_train.isna().sum(axis=0) / len(df_train), 4) * 100)
 print(np.round(df_test.isna().sum(axis=0) / len(df_test), 4) * 100)
